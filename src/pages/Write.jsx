76f5d49ee8cd534fc79a1ea32f58611fb7b7bb27
Belal-hossainn <belalhossain.rn@gmail.com>
2022-11-01 00:38:19 +0600

    write page done

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
+  const [value, setValue] = useState("");
+
+  console.log(value)
   return (
-    <div>Write</div>
+    <div className='add'>
+     <div className="content">
+      <input type="text" placeholder='title'/>
+      <div className="editorContainer">
+      <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+      </div>
+     </div>
+     <div className="menu">
+      <div className="item">
+        <h1>Publish</h1>
+        <span>
+          <b>Status: </b> draft
+        </span>
+        <span>
+          <b>Visibility: </b> public
+        </span>
+        <input style={{display:"none"}} type="file" name="" id="file" />
+        <label className="file" htmlFor="file">Upload image</label>
+        <div className="buttons">
+          <button>save as draft</button>
+          <button>update</button>
+        </div>
+      </div>
+      <div className="item">
+        <h1>Category</h1>
+        <div className="cat">
+        <input type="radio" name="cat" value='art' id="art" />
+        <label htmlFor="art">Art</label>
+        </div>
+        <div className="cat">
+        <input type="radio" name="cat" value='literature' id="literature" />
+        <label htmlFor="literature">Literature</label>
+        </div>
+        <div className="cat">
+        <input type="radio" name="cat" value='movies' id="movies" />
+        <label htmlFor="movies">Movies</label>
+        </div>
+        <div className="cat">
+        <input type="radio" name="cat" value='music' id="music" />
+        <label htmlFor="music">Music</label>
+        </div>
+        
+      </div>
+     </div>
+    </div>
   )
 }
 
